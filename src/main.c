17a608de87e973d23b1a8f5caf575f38d2868f82
TheCDeveloper <230489966+TheCDeveloper@users.noreply.github.com>
2026-03-17 17:00:49 +0530

    feat(main): add logs

--- src/main.c
+++ src/main.c
@@ -1,14 +1,20 @@
 #include "game.h"
+#include "log.h"
 
 
 int main(int argc, char *argv[]) {
+    LOG_PRINT("version: 0.1.0");
+
     Game *game = game_create();
 
     if (!game) {
+        LOG_ERROR("exiting...");
         return 1;
     }
 
     game_run(game);
+
+    LOG_PRINT("quitting");
     game_destroy(game);
     return 0;
 }
